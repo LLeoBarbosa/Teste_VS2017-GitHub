39a3dc6791e7655bd17fea1bab75bd4ca6577d25
Leo Barbosa <leobarbosa.ti@gmail.com>
2020-10-06 10:10:21 -0300

    Alteração no arquivo de codigo Program.cs

--- TesteConsoleApp/TesteConsoleApp/Program.cs
+++ TesteConsoleApp/TesteConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace TesteConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World, Git HUm On the Area!");
+            Console.WriteLine("Hello World, Git Hub On the Area!");
         }
     }
 }
